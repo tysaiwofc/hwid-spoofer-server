52f96eb79d468aa13b07773e49308294b60be561
tysaiwofc <machadomarcos2019@gmail.com>
2025-04-24 09:09:42 -0300

    updated2

--- src/app/api/update/route.ts
+++ src/app/api/update/route.ts
@@ -2,7 +2,8 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   return NextResponse.json({
-    version: "1.0.5",
+    version: "1.0.7",
+    changelog: "Melhorias no desempenho geral.",
     url: "https://hwid-spoofer-server.vercel.app/update.zip"
   });
 }
